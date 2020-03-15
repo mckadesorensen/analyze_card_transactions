eef09495246bfd26ab9fa91024a068276e478749
McKade <mckades@gmail.com>
2020-03-15 12:22:01 -0800

    Added cleanup_merchant

--- parse_transactions.py
+++ parse_transactions.py
@@ -1,4 +1,15 @@
+from typing import List
+
 from pull_monthy_transactions_from_csv import get_monthly_transactions
+
+
+def cleanup_merchant() -> List:
+    return []
+
+
+def main():
+    return []
+
 
 if __name__ == "__main__":
     pass
