b25434d8baf9e7bcd3ac1388359f374043973175
McKade <mckades@gmail.com>
2020-03-15 13:34:44 -0800

    Finished function that cleans up merchant names

--- parse_transactions.py
+++ parse_transactions.py
@@ -1,15 +1,26 @@
-from typing import List
+from typing import List, Dict
+import re
 
-from pull_monthy_transactions_from_csv import get_monthly_transactions
-
-
-def cleanup_merchant() -> List:
-    return []
+from .pull_monthy_transactions_from_csv import get_monthly_transactions
 
 
-def main():
-    return []
+def cleanup_merchant(transactions: List[Dict]) -> List:
+    new_transactions = []
+    NUMBERS = re.compile(r"(.*) ([0-9]+)")
+    for transaction in transactions:
+        m = re.match(NUMBERS, transaction["merchant"])
+        if m:
+            merchant, _ = m.groups()
+            transaction["merchant"] = merchant
+        new_transactions.append(transaction)
+
+    return new_transactions
+
+
+def main() -> List:
+    return cleanup_merchant(get_monthly_transactions())
 
 
 if __name__ == "__main__":
-    pass
+    for t in main():
+        print(t["merchant"])
